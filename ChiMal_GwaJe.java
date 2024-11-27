098092564560f4173df215fe8caa366fe669dc90
OMS <s92225705@gmail.com>
2024-11-28 03:46:36 +0900

    Add files via upload

---
+++ ChiMal_GwaJe.java
@@ -0,0 +1,20 @@
+package vaa;
+
+import javax.swing.JFrame;
+
+public class ChiMal_GwaJe extends JFrame{
+	
+	ChiMal_GwaJe() {
+		setTitle("기말 과제");
+		setSize(500,500);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+
+	public static void main(String[] args) {
+		new ChiMal_GwaJe();
+	}
+
+}
